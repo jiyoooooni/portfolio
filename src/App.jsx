74fd663e4ada0e48367840f33012f24cbe22bf0e
jiyoooooni <pooooooop@naver.com>
2019-03-29 20:47:59 +0900

    init project environment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './stylesheets/app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrap">
+        {/* HTML 역할 */}
+        <p>It's Jiyoon's Application</p>
+      </div>
+    );
+  }
+}
+
+export default App;
