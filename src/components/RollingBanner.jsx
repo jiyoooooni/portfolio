77b42813403ee7b429baa88ff5cd79477c892973
jiyoooooni <pooooooop@naver.com>
2019-05-02 14:41:40 +0900

    add Rolling banner image

--- src/components/RollingBanner.jsx
+++ src/components/RollingBanner.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function RollingBanner() {
-  return <div className="RollingBanner" />;
+  return <div className="RollingBanner">
+    <ul className="visualbx">
+      <li className="visual"></li>
+      <li className="visual"></li>
+      <li className="visual"></li>
+    </ul>
+</div>;
 }
 
 export default RollingBanner;
